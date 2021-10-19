825d976328f18420e4966bef82165dde3c39b358
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-20 00:31:42 +0530

    moved async to outside from useEffect

--- react-chucknorris-api/src/App.js
+++ react-chucknorris-api/src/App.js
@@ -5,41 +5,47 @@ import axios from 'axios';
 
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
+    fetchData();
 
-axios.get('https://api.chucknorris.io/jokes/random')
-  },[])
+  }, [])
+
+  const fetchData = async () => {
+  const result=  await axios.get('https://api.chucknorris.io/jokes/random');
+    console.log(result.data.value);
+
+  }
   return (
-   <div className="container">
-     <div className="row">
-       <div className="col-6">
-         <h1 className="title">
-           Search a Joke 
-         </h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="title">
+            Search a Joke
+          </h1>
 
-       </div>
-       <div className="col-6searchJokeCol">
-         <div className="card">
-           <div className="card-header">
-           Search for a word
-               </div>
-<div className="card-body">
-  <input type="text"></input>
+        </div>
+        <div className="col-6searchJokeCol">
+          <div className="card">
+            <div className="card-header">
+              Search for a word
+            </div>
+            <div className="card-body">
+              <input type="text"></input>
 
-</div>
-         </div>
-         <button className="btn btn-warning btn-lg">Generate Joke</button>
-         
-       </div>
-     </div>
-     <h2 className="subtitle">
-Here is the joke
-     </h2>
-     <h4 >
-Hi this is the joke
-     </h4>
-     
-   </div>
+            </div>
+          </div>
+          <button className="btn btn-warning btn-lg">Generate Joke</button>
+
+        </div>
+      </div>
+      <h2 className="subtitle">
+        Here is the joke
+      </h2>
+      <h4 >
+        Hi this is the joke
+      </h4>
+
+    </div>
   );
 }
 
